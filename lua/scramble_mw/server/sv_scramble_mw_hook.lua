e075be1a5c3fe386ccfe11ada9970132e6e8f3a6
MrMarant <laughymclaugher@gmail.com>
2023-09-12 19:23:33 +0200

    [UPDATE] New hook detection Guth

--- lua/scramble_mw/server/sv_scramble_mw_hook.lua
+++ lua/scramble_mw/server/sv_scramble_mw_hook.lua
@@ -2,8 +2,19 @@ if CLIENT then return end
 
 local CacheIsScrambleEnable = true
 
+--! Old Hook for the SCP-096 detection
 -- Hook from Guth swep to be detected by SCP 096
-hook.Add( "vkxscp096:should_trigger", "Scramble_MW_Detect_SCP096", function(target, ply)
+hook.Add( "vkxscp096:should_trigger", "vkxscp096_Scramble_MW_Detect_SCP096", function(target, ply)
+    if (target.vrnvgflipped and !target.vrnvgbroken and target.nvgbattery > 0 and SCRAMBLE_MW_CONFIG.IsScrambleEnable:GetBool()) then
+        if (!scramble_mw.IsDetectedBySCP096()) then
+            return false
+        end
+    end
+end)
+
+--? New Hook for the SCP-096 detection
+-- Hook from Guth swep to be detected by SCP 096
+hook.Add( "guthscp096:should_trigger", "guthscp096_Scramble_MW_Detect_SCP096", function(target, ply)
     if (target.vrnvgflipped and !target.vrnvgbroken and target.nvgbattery > 0 and SCRAMBLE_MW_CONFIG.IsScrambleEnable:GetBool()) then
         if (!scramble_mw.IsDetectedBySCP096()) then
             return false
