7cc86f9bc9a7d32a2e5184673b36bc75c9b4f5bb
MrMarrant <laughymclaugher@gmail.com>
2025-04-15 21:13:16 +0200

    [FIX] Wrong references

--- lua/scramble_mw/config/sh_scramble_mw_config.lua
+++ lua/scramble_mw/config/sh_scramble_mw_config.lua
@@ -52,28 +52,28 @@ SCRAMBLE_MW_CONFIG.ModelName["models/cpthazama/scp/096_old.mdl"] = {
     z = 3,
     scale = 0.7
 }
-SCRAMBLE_CONFIG.ModelName["models/scpsl/dughoo/dughoo_scp096.mdl"] = {
+SCRAMBLE_MW_CONFIG.ModelName["models/scpsl/dughoo/dughoo_scp096.mdl"] = {
     head = "head",
     x = 0,
     y = 2,
     z = 3,
     scale = 0.8
 }
-SCRAMBLE_CONFIG.ModelName["models/elan/scpsl/096/096.mdl"] = {
+SCRAMBLE_MW_CONFIG.ModelName["models/elan/scpsl/096/096.mdl"] = {
     head = "Head",
     x = 8,
     y = 0,
     z = 0,
     scale = 0.7
 }
-SCRAMBLE_CONFIG.ModelName["models/painkiller_76/sf2/new096/096_v2.mdl"] = {
+SCRAMBLE_MW_CONFIG.ModelName["models/painkiller_76/sf2/new096/096_v2.mdl"] = {
     head = "head",
     x = 0,
     y = 0,
     z = 0,
     scale = 0.9
 }
-SCRAMBLE_CONFIG.ModelName["models/painkiller_76/sf2/new096/096b.mdl"] = {
+SCRAMBLE_MW_CONFIG.ModelName["models/painkiller_76/sf2/new096/096b.mdl"] = {
     head = "head",
     x = 0,
     y = 0,
